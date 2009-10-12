ad19af02a2bb06a2731e9942138bee3c30c6b264
Sean Nelson <snelson@AudiohackedMac.local>
2009-10-11 22:17:16 -0700

    commit more of what i have Signed-off-by: Sean Nelson <audiohacked@gmail.com>

---
+++ pyBusPirate/XModem/receive.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Created by Sean Nelson on 2009-09-20.
+Copyright 2009 Sean Nelson <audiohacked@gmail.com>
+
+This file is part of pyBusPirate.
+
+pyBusPirate is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pyBusPirate is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyBusPirate.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from enum import ControlChar
+from transmission_medium import XComm
+
+MAXRETRANS=25
+
+class RecvMsgBlock:
+	""" Message Block format: Receive """
+	retrans=25
+	retry=0
+	packetno=1
+	bufsz = 0
+	trychar = 'C'
+	mlen = 0
+	dest=''
+	rfile = None
+	
+	def __init__(self, of=None):
+		XComm.__init__(self)
+		self.rfile = of
+
+	def check(self, crc, string, size):
+		if crc:
+			crc = crc16(string,size)
+			tcrc = (string[size]<<8)+string[size+1]
+			if crc is tcrc: return 1
+		else:
+			cks = 0
+			for i in range(size): cks += string[i]
+			if cks is string[size]: return 1
+		return 0
+
+	def receive(self):
+		crc = 0
+		for retry in range(16):
+			if self.trychar: self.outbyte(self.trychar)
+			c = self.inbyte()
+			if c is ControlChar.SOH:
+				self.bufsz = 128
+				self.start_recv()
+			elif c is ControlChar.STX:
+				self.bufsz = 1024
+				self.start_recv()
+			elif c is ControlChar.EOT:
+				self.outbyte(ControlChar.ACK)
+			elif c is ControlChar.CAN:
+				if self.inbyte() is ControlChar.CAN: self.outbyte(ControlChar.ACK)
+			else:
+				if self.trychar is 'C':
+					self.trychar = ControlChar.NAK
+					continue
+				self.outbyte(ControlChar.CAN)
+				self.outbyte(ControlChar.CAN)
+				self.outbyte(ControlChar.CAN)
+				return None
+
+	def start_recv(self):
+		if self.trychar is 'C': crc = 1
+		self.trychar = 0
+		xbuff = ''
+		xbuff[0] = c
+		dest = ''
+		for i in range(self.bufsz+crc+3):
+			c = self.self.inbyte(DLY_1S)
+			if not c: self.reject()
+			xbuff[i] = c
+		if (xbuff[1] == ~xbuff[2]) and (xbuff[1] == packetno or xbuff[1] == (packetno-1)) and 		check(crc, xbuff[3:], self.bufsz):
+			if (xbuff[1] == packetno):
+				count = destsz - slen
+				if count > self.bufsz: count = self.bufsz
+				if count > 0:
+					dest += xbuff[3:count+3]
+					slen += count
+				++packetno
+				self.retrans = MAXRETRANS+1
+			if --self.retrans <= 0:
+				self.outbyte(ControlChar.CAN)
+				self.outbyte(ControlChar.CAN)
+				self.outbyte(ControlChar.CAN)
+				return -3
+			self.outbyte(ControlChar.ACK)
+
+	def reject(self):
+		self.outbyte(ControlChar.NAK)
+
+	def write_file(self):
+		pass
---
+++ pyBusPirate/XModem/transfer.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Created by Sean Nelson on 2009-09-20.
+Copyright 2009 Sean Nelson <audiohacked@gmail.com>
+
+This file is part of pyBusPirate.
+
+pyBusPirate is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pyBusPirate is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyBusPirate.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from enum import ControlChar
+from transmission_medium import XComm
+
+MAXRETRANS=25
+
+class TransMsgBlock(XComm):
+	""" Message Block Format: Transmit """
+	mlen = 0
+	crc = -1
+	packetno = 1
+	tdata = ''
+	tsize = 0
+	
+	def __init__(self):
+		XComm.__init__(self)
+
+	def transmit(self, data, size):
+		self.tdata = data
+		self.tsize = size
+		while 1:
+			for retry in range(16):
+				c = self.inbyte()
+				if c is 'C':
+					crc = 1
+					self.start_trans()
+				elif c is ControlChar.NAK:
+					crc = 0
+					self.start_trans()
+				elif c is ControlChar.CAN:
+					if self.inbyte() is ControlChar.CAN:
+						self.outbyte(ControlChar.ACK)
+						return None
+				else:
+					self.outbyte(ControlChar.CAN)
+					self.outbyte(ControlChar.CAN)
+					self.outbyte(ControlChar.CAN)
+					return None
+
+	def start_trans(self):
+		self.tsize = 128
+		xbuff = ''
+		xbuff[0] = SOH
+		xbuff[1] = packetno
+		xbuff[2] = ~packetno
+		c = self.tsize - self.mlen
+		if c > self.tsize: c = self.tsize
+		if c >= 0:
+			xbuf[3:self.tsize+3] = ''
+			if c is 0: xbuff[3] = ControlChar.CTRLZ
+			else:
+				xbuff[3:c] = self.tdata[self.mlen:c]
+				if c < self.tsize: xbuff[3+c] = ControlChar.CTRLZ
+			if crc:
+				ccrc = crc16(xbuff[3], self.tsize)
+				xbuff[self.tsize+3] = (ccrc>>8) & 0xFF
+				xbuff[self.tsize+4] = ccrc & 0xFF
+			else:
+				ccks = 0
+				for i in range(3,self.tsize+3): ccks += xbuff[i]
+				xbuff[self.tsize+3] = ccks
+			
+			for retry in range(MAXRETRANS):
+				for i in range(self.tsize+4+crc): self.outbyte(xbuff[i])
+				c = self.inbyte()
+				if c is ControlChar.ACK:
+					++packetno
+					self.mlen += self.tsize
+					return start_trans(data,size)
+				elif c is ControlChar.CAN:
+					if self.inbyte() == ControlChar.CAN:
+						self.outbyte(ControlChar.ACK)
+						return None
+				elif ControlChar.NAK:
+					return None
+				else:
+					return None
+			self.outbyte(ControlChar.CAN)
+			self.outbyte(ControlChar.CAN)
+			self.outbyte(ControlChar.CAN)
+			return None
+		else:
+			for retry in range(10):
+				self.outbyte(ControlChar.EOT)
+				c = self.inbyte()
+				if c is ControlChar.ACK: break
+			if c is ControlChar.ACK: return self.mlen
+			else: return None
